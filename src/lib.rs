3d9ff8d350fcdc3ec33f4d4dc7b9afc4831a8a83
suniljose <bsuniljose25@gmail.com>
2024-02-29 19:46:19 +0530

    Add subscribe endpoint with basic form handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use actix_web::dev::Server;
+use actix_web::web::Form;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
@@ -6,10 +7,24 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
