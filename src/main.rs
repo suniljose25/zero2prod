5ef66eda212c203e9ce9e6d65d061ac54b638301
suniljose <bsuniljose25@gmail.com>
2024-03-02 22:34:54 +0530

    Update main method to use Layers and subscriber for json styled logs.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use env_logger::Env;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use zero2prod::startup::run;
 
@@ -9,7 +12,17 @@ mod configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // redirect all log events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
+    
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
